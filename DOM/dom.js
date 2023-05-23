878f0e4705e97a93ef2f6ab25954fe7645b93d5a
Deenk22 <smj_personal@hotmail.com>
2023-05-23 13:53:02 +0200

    Inicio Sesion

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,8 +1,9 @@
 function inicializar() {
+
   console.log(`Onload se ha llamado`);
   const divContainer = document.getElementById("container");
   divContainer.innerHTML = "<p>Div modificado</p>";
-  //recorrer(users, soloName);
+  
 
   const users = [
     {
@@ -49,21 +50,49 @@ function inicializar() {
     },
   ];
 
-  // function recorrer(array, names) {
-  //   for (const item of array) {
-  //     names(item);
-  //   }
-  // }
-
-  // function soloName(item) {
-  //   let { name, age } = item;
-  //   console.log(name, age);
-  // }
-
   const result = [];
   for (const item of users) {
-    divContainer.innerHTML += `<li>El nombre es ${item.name}</li>`;
-  }
+    divContainer.innerHTML += `<li>Nombre:<strong>${item.name}</strong>, Edad:<strong>${item.age}</strong> </li>`;
+  };
 
   console.log(result);
-}
+
+};
+
+
+function login (){
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const error = document.getElementById("error");
+  const iniciarSesion = document.getElementById("iniciarSesion");
+
+
+  if (email === "smj_personal@hotmail.com" && password === "12345"){
+    iniciarSesion.innerHTML = "<p>Has iniciado sesion</p>"
+    console.log(`Has iniciado sesion`);
+  } else {
+    error.innerHTML = "<p>Email o contraseña incorrectos</p>"
+    console.log(`Algo mal estas haciendo`);
+  };
+
+
+  // if (email === "smj_personal@hotmail.com"){
+  //   console.log(`Correcto`);
+  // } else {
+  //   errorEmail.innerHTML = "<p>Correo incorrecto</p>"
+  // };
+
+  // if (password === "12345"){
+  //   console.log(`Correcto`);
+  // } else {
+  //   errorPassword.innerHTML = "<p>Contraseña Incorrecta/p>"
+  // }
+
+};
+
+
+
+
+
+
